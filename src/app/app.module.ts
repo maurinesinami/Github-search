409fb164ca18f9ecda9ff8f8a2159e52cd945726
maurinenami <maurinenami>
2019-05-04 14:31:33 +0300

    add new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
